6774d17ddd659c7d1f52c253665f4d26da7d5621
jakedahn <jakedahn@gmail.com>
2023-02-10 14:51:49 -0700

    a little bit of autoformat

--- qa.py
+++ qa.py
@@ -7,8 +7,10 @@ from langchain import OpenAI
 from langchain.chains import VectorDBQAWithSourcesChain
 
 
-parser = argparse.ArgumentParser(description='Ask a question to the cloudflare docs.')
-parser.add_argument('question', type=str, help='The question to ask the cloudflare docs')
+parser = argparse.ArgumentParser(description="Ask a question to the cloudflare docs.")
+parser.add_argument(
+    "question", type=str, help="The question to ask the cloudflare docs"
+)
 args = parser.parse_args()
 
 
@@ -18,7 +20,10 @@ with open("cloudflare_docs.pkl", "rb") as f:
     store = pickle.load(f)
 
 store.index = index
-chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0, max_tokens=1500, model_name='text-davinci-003'), vectorstore=store)
+chain = VectorDBQAWithSourcesChain.from_llm(
+    llm=OpenAI(temperature=0, max_tokens=1500, model_name="text-davinci-003"),
+    vectorstore=store,
+)
 result = chain({"question": args.question})
 print(f"Answer: {result['answer']}")
 print(f"Sources: {result['sources']}")
